3acaf801bc43ba6f36f9a0cb7dcf754160f71e6a
Zakaria Mujur Prasetyo <zakariamujur6@gmail.com>
2026-06-21 20:17:30 +0700

    style: enlarge mobile hero illustration and adjust top spacing overlay

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -21,9 +21,9 @@ export default function HeroSection() {
           <div className="relative flex-1 max-w-[742px] pt-0 sm:pt-6 lg:pt-16">
             
             {/* Wrapper Heading, Description & Character overlay */}
-            <div className="relative pt-[240px] xs:pt-[280px] lg:pt-0 mb-4 sm:mb-6">
+            <div className="relative pt-[150px] xs:pt-[185px] lg:pt-0 mb-4 sm:mb-6">
               {/* Ilustrasi Mobile-only (di atas hello world secara vertikal, tapi di belakang overlay-nya) */}
-              <div className="block lg:hidden absolute top-[10px] left-1/2 -translate-x-1/2 w-[360px] xs:w-[400px] max-w-[130%] opacity-100 pointer-events-none z-10">
+              <div className="block lg:hidden absolute top-[-20px] left-1/2 -translate-x-1/2 w-[400px] xs:w-[460px] max-w-[125%] opacity-100 pointer-events-none z-10">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                   src="/images/person-learn-coding.svg"
